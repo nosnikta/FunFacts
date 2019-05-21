8723ed1afc1d3bfbabaff50c0211a97be6db418e
Ben <benaatkinson@gmail.com>
2019-05-21 14:58:15 +1000

    Removed the "Hi"

--- app/src/main/java/com/example/benaa/funfacts/FunFactsMainActivity.java
+++ app/src/main/java/com/example/benaa/funfacts/FunFactsMainActivity.java
@@ -26,7 +26,7 @@ public class FunFactsMainActivity extends AppCompatActivity {
                 Random randomGenerator = new Random();
 
                 int randomNumber = randomGenerator.nextInt(3);
-                String fact = randomNumber + "Hi";
+                String fact = randomNumber + "";
                 factTextView.setText(fact);
             }
         };
