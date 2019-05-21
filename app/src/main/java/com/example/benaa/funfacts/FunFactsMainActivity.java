e1067880bab6bc6e3e7cce82f0dbf870a6be1039
Ben Atkinson <benaatkinson@gmail.com>
2019-05-21 19:41:33 +1000

    Added the array!!

--- app/src/main/java/com/example/benaa/funfacts/FunFactsMainActivity.java
+++ app/src/main/java/com/example/benaa/funfacts/FunFactsMainActivity.java
@@ -21,12 +21,25 @@ public class FunFactsMainActivity extends AppCompatActivity {
         showFactButton = (Button) findViewById(R.id.showFactButtton);
 
         View.OnClickListener listener = new View.OnClickListener() {
+
+            String[] facts = {
+                    "Ants stretch when they wake up in the morning.",
+                    "Ostriches can run faster than horses.",
+                    "Olympic gold medals are actually made mostly of silver.",
+                    "You are born with 300 bones; by the time you are an adult you will have 206.",
+                    "It takes about 8 minutes for light from the Sun to reach Earth.",
+                    "Some bamboo plants can grow almost a meter in just one day.",
+                    "The state of Florida is bigger than England.",
+                    "Some penguins can leap 2-3 meters out of the water.",
+                    "On average, it takes 66 days to form a new habit.",
+                    "Mammoths still walked the earth when the Great Pyramid was being built." };
+
             @Override
             public void onClick(View v) {
                 Random randomGenerator = new Random();
 
-                int randomNumber = randomGenerator.nextInt(3);
-                String fact = randomNumber + "";
+                int randomNumber = randomGenerator.nextInt(10);
+                String fact = facts[randomNumber];
                 factTextView.setText(fact);
             }
         };
