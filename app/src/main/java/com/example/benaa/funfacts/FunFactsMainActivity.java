f4e6b935426c55bdb69f7e53d21591c2f00f4656
Ben <benaatkinson@gmail.com>
2019-05-21 14:45:05 +1000

    Initial commit

---
+++ app/src/main/java/com/example/benaa/funfacts/FunFactsMainActivity.java
@@ -0,0 +1,39 @@
+package com.example.benaa.funfacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class FunFactsMainActivity extends AppCompatActivity {
+    private TextView factTextView;
+    private Button showFactButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts_main);
+        //Make sure the is an element with the id in the xml layout.
+        factTextView = (TextView) findViewById(R.id.factTextView);
+        showFactButton = (Button) findViewById(R.id.showFactButtton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomGenerator = new Random();
+
+                int randomNumber = randomGenerator.nextInt(3);
+                String fact = randomNumber + "";
+                factTextView.setText(fact);
+            }
+        };
+
+        showFactButton.setOnClickListener(listener);
+
+
+    }
+
+}
